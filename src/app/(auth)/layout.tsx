3a5eee9d6a3ccee08b849a9ab210e059f033b19d
Maximiliano Bisurgi <maximiliano.bisurgi@gmail.com>
2025-06-04 12:09:33 -0300

    Remove browser default zoom

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import { useEffect } from "react";
+
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    const preventZoom = (e: WheelEvent) => {
+      if (e.ctrlKey) {
+        e.preventDefault();
+      }
+    };
+
+    window.addEventListener("wheel", preventZoom, { passive: false });
+
+    return () => {
+      window.removeEventListener("wheel", preventZoom);
+    };
+  }, []);
+
   return <>{children}</>;
 }
