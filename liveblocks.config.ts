b9ddcc3b7b126b35c2635e71513de6f949e38639
mbisurgisoshace <maximiliano.bisurgi@gmail.com>
2025-06-20 11:25:12 -0300

    Multiple selection and grouping

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -2,7 +2,7 @@
 
 import { LiveList } from "@liveblocks/client";
 
-import { Arrow, CanvasBlock } from "@/types";
+import { Arrow, CanvasBlock, Group } from "@/types";
 
 // https://liveblocks.io/docs/api-reference/liveblocks-react#Typing-your-data
 declare global {
@@ -16,6 +16,7 @@ declare global {
     // The Storage tree for the room, for useMutation, useStorage, etc.
     Storage: {
       arrows: LiveList<Arrow>;
+      groups: LiveList<Group>;
       items: LiveList<CanvasBlock>;
     };
 
